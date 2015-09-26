d47d2142f441a2567a6d1ab1b112f179e07acd91
Ben Meline <benmeline@gmail.com>
2015-09-25 18:05:13 -0700

    PureLayout

--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ use_frameworks!
 pod 'Alamofire', '~> 1.3'
 pod 'SwiftyJSON', '~> 2.2.1'
 pod 'MagicalRecord', '~> 2.3.0'
+pod 'PureLayout', '~> 3.0.1'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -54,10 +54,12 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/MagicalRecord.framework'
+  install_framework 'Pods/PureLayout.framework'
   install_framework 'Pods/SwiftyJSON.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/MagicalRecord.framework'
+  install_framework 'Pods/PureLayout.framework'
   install_framework 'Pods/SwiftyJSON.framework'
 fi
