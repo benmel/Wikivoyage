c643d6f517dbc5a0ea85668d748ee8d0124c90fd
Ben Meline <benmeline@gmail.com>
2015-10-05 22:54:14 -0700

    Table view code style

--- Wikivoyage/FavoritesTableViewController.swift
+++ Wikivoyage/FavoritesTableViewController.swift
@@ -11,108 +11,60 @@ import MagicalRecord
 
 class FavoritesTableViewController: UITableViewController {
 
-    var savedPages: [SavedPage] = []
+    var favoritePages = [SavedPage]()
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
-        savedPages = SavedPage.MR_findByAttribute("favorite", withValue: true) as! [SavedPage]
+        navigationItem.rightBarButtonItem = editButtonItem()
+        favoritePages = SavedPage.MR_findByAttribute("favorite", withValue: true, andOrderBy: "title", ascending: true) as! [SavedPage]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 1
-    }
+    // MARK: - Table View Data Source
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return savedPages.count
+        return favoritePages.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("SavedPage", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-        let savedPage = savedPages[indexPath.row]
-        cell.textLabel?.text = savedPage.title
-        
+        let cell = tableView.dequeueReusableCellWithIdentifier("FavoritePage", forIndexPath: indexPath) as! UITableViewCell
+        let favoritePage = favoritePages[indexPath.row]
+        cell.textLabel?.text = favoritePage.title
         return cell
     }
-        
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let savedPage = savedPages[indexPath.row]
-        performSegueWithIdentifier("ShowWeb", sender: savedPage)
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    // Override to support editing the table view.
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            let savedPage = savedPages[indexPath.row]
-            if savedPage.offline == true {
-                savedPage.favorite = false
-                savedPages.removeAtIndex(indexPath.row)
+            let favoritePage = favoritePages[indexPath.row]
+            if favoritePage.offline == true {
+                // If the page is also offline, only update favorite property
+                favoritePage.favorite = false
+                favoritePages.removeAtIndex(indexPath.row)
             } else {
-                savedPages.removeAtIndex(indexPath.row).MR_deleteEntity()
+                favoritePages.removeAtIndex(indexPath.row).MR_deleteEntity()
             }
             NSManagedObjectContext.MR_defaultContext().MR_saveToPersistentStoreAndWait()
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-        if segue.identifier == "ShowWeb" {
-            let viewController = segue.destinationViewController as! LocationWebViewController
-            let savedPage = sender as! SavedPage
-            viewController.pageId = Int(savedPage.id)
-            viewController.pageTitle = savedPage.title
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
         }
     }
 
+    // MARK: - Table View Delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let favoritePage = favoritePages[indexPath.row]
+        performSegueWithIdentifier("ShowWeb", sender: favoritePage)
+    }
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowWeb" {
+            let vc = segue.destinationViewController as! LocationWebViewController
+            let favoritePage = sender as! SavedPage
+            vc.pageId = Int(favoritePage.id)
+            vc.pageTitle = favoritePage.title
+        }
+    }
 }
